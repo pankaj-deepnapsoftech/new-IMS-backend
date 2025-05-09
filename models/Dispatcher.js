a27523ef84b4869f5aa983523fd08153a0f3dbe9
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-05-09 15:55:26 +0530

    some changes

--- models/Dispatcher.js
+++ models/Dispatcher.js
@@ -4,6 +4,7 @@ const DispatchSchema = new Schema({
     Sale_id: { type: Schema.Types.ObjectId, ref: "purchase" },
     tracking_id: { type: String,  trim: true },
     tracking_web: { type: String, trim: true },
+    creator:{type:Schema.Types.ObjectId,ref:"User"},
     delivery_status: { type: String, enum: ["Dispatch", "Delivered"] },
     Task_status: { type: String, enum: ["Pending", "Processing", "Completed"],default:"Pending" }
 });
--- routes/Dispatch.routes.js
+++ routes/Dispatch.routes.js
@@ -1,12 +1,13 @@
 
 const { Router } = require("express");
 const { isAuthenticated } = require("../middlewares/isAuthenticated");
-const { CreateDispatch, DeleteDispatch } = require("../controllers/dispatch.controller");
+const { CreateDispatch, DeleteDispatch, GetDispatch } = require("../controllers/dispatch.controller");
 
 const routes = Router();
 
 routes.route("/createDispatch").post(isAuthenticated, CreateDispatch);
 routes.route("/Delete-Dispatch/:id").post(isAuthenticated, DeleteDispatch);
+routes.route("/get-Dispatch").get(isAuthenticated, GetDispatch);
 routes.route("/update-Dispatch/:id").put(isAuthenticated, UpdateDispatch);
 
 
