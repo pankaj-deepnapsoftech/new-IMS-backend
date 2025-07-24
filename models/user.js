48956ec14b11296a69b6faf97df62b94d728a05e
vishalpanchal1024 <vishalpanchal1024@gmail.com>
2025-07-24 14:21:09 +0530

    first

--- models/user.js
+++ models/user.js
@@ -1,78 +1,86 @@
-const {Schema, model} = require("mongoose");
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     first_name: {
-        type: String,
-        required: [true, "First Name is a required field"],
-        minlength: [2, "First Name must be atleast 2 characters long"],
-        maxlength: [40, "First Name cannot exceed 40 characters"],
+      type: String,
+      required: [true, "First Name is a required field"],
+      minlength: [2, "First Name must be atleast 2 characters long"],
+      maxlength: [40, "First Name cannot exceed 40 characters"],
     },
     last_name: {
-        type: String,
-        maxlength: [40, "Last Name cannot exceed 40 characters"],
+      type: String,
+      maxlength: [40, "Last Name cannot exceed 40 characters"],
     },
     email: {
-        type: String,
-        required: [true, "Email is a required field"],
-        unique: true,
-        match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address']
+      type: String,
+      required: [true, "Email is a required field"],
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, "Please provide a valid email address"],
     },
     phone: {
-        type: String,
-        required: [true, 'Phone is a required field'],
-        unique: true,
-        match:  [/^[7-9]\d{9}$/, 'Please provide a valid Indian mobile number']
+      type: String,
+      required: [true, "Phone is a required field"],
+      unique: true,
+      match: [/^[7-9]\d{9}$/, "Please provide a valid Indian mobile number"],
     },
     isSuper: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
+    },
+    employeeId: {
+      type: String,
+      unique: true,
+      sparse: true,
     },
     role: {
-        type: Schema.Types.ObjectId,
-        ref: "User-Role"
+      type: Schema.Types.ObjectId,
+      ref: "User-Role",
     },
     password: {
-        type: String,
-        required: [true, 'Password is a required field'],
-        minlength: [8, 'Password must be atleast 8 digits long'],
-        select: false
+      type: String,
+      required: [true, "Password is a required field"],
+      minlength: [8, "Password must be atleast 8 digits long"],
+      select: false,
     },
     isVerified: {
-        type: Boolean,
-        default: false
-    }
-}, {
-    timestamps: true
-});
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
-    if(!this.isModified('password')){
-        return next();
-    }
+  if (!this.isModified("password")) {
+    return next();
+  }
 
-    try {
-        const hashedPass = await bcrypt.hash(this.password, 10);
-        this.password = hashedPass;
-        next();
-    } catch (error) {
-        next(error);
-    }
-})
+  try {
+    const hashedPass = await bcrypt.hash(this.password, 10);
+    this.password = hashedPass;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 userSchema.pre("findOneAndUpdate", async function (next) {
-    if(!this._update.password){
-        return next();
-    }
+  if (!this._update.password) {
+    return next();
+  }
 
-    try {
-        const hashedPass = await bcrypt.hash(this._update.password, 10);
-        this._update.password = hashedPass;
-        next();
-    } catch (error) {
-        next(error);
-    }
-})
+  try {
+    const hashedPass = await bcrypt.hash(this._update.password, 10);
+    this._update.password = hashedPass;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 const User = model("User", userSchema);
 module.exports = User;
