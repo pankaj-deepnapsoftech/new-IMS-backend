8aa1cc0d43b0de8ca1209830f3afac5a18141b6e
vishalpanchal1024 <vishalpanchal1024@gmail.com>
2025-08-02 16:57:12 +0530

    bulk--direct

--- routes/product.js
+++ routes/product.js
@@ -1,17 +1,40 @@
 const express = require("express");
-const { create, update, remove, details, all, unapproved, bulkUploadHandler, workInProgressProducts } = require("../controllers/product");
+const { 
+  create, 
+  update, 
+  remove, 
+  details, 
+  all, 
+  unapproved, 
+  bulkUploadHandler, 
+  workInProgressProducts,
+  exportToExcel,
+  downloadSampleTemplate
+} = require("../controllers/product");
 const { isAuthenticated } = require("../middlewares/isAuthenticated");
 const { isSuper } = require("../middlewares/isSuper");
 const { isAllowed } = require("../middlewares/isAllowed");
 const { upload } = require("../utils/upload");
 const router = express.Router();
 
-router.route("/").post(isAuthenticated, isAllowed, create).put(isAuthenticated, isAllowed, update).delete(isAuthenticated, isAllowed, remove);
+// CRUD operations
+router.route("/")
+  .post(isAuthenticated, isAllowed, create)
+  .put(isAuthenticated, isAllowed, update)
+  .delete(isAuthenticated, isAllowed, remove);
+
+// Get operations
 router.get("/all", isAuthenticated, all);
 router.get("/wip", isAuthenticated, workInProgressProducts);
 router.get("/unapproved", isAuthenticated, isSuper, unapproved);
-router.post("/bulk", isAuthenticated, upload.single('excel'), bulkUploadHandler);
 router.get("/:id", isAuthenticated, isAllowed, details);
 
+// Bulk operations
+router.post("/bulk", isAuthenticated, isAllowed, upload.single('excel'), bulkUploadHandler);
+
+// Export operations - Updated for direct products 
+
+router.get("/export/excel", isAuthenticated, exportToExcel);
+router.get("/export/sample", downloadSampleTemplate);
+
 module.exports = router;
- 
